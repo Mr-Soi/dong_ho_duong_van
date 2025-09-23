ab3843652496d6c990d958c4df8812999fbaf1cc
Mr-Soi <mr.soija66@gmail.com>
2025-09-23 16:04:37 +0700

    DHDV: silence HTTP_PORTS warn, static cache, smoker logs, nightly backup

---
+++ scripts/backup_dhdv.ps1
@@ -0,0 +1,4 @@
+﻿$ts=(Get-Date -Format yyyyMMdd_HHmm);
+docker exec dhdv_sql /opt/mssql-tools18/bin/sqlcmd -C -S localhost -d master -U sa -P 'S@f3Pass_2025!' -Q "BACKUP DATABASE [dhdv] TO DISK=N'/var/opt/mssql/backup/dhdv_$ts.bak' WITH INIT";
+docker cp dhdv_sql:/var/opt/mssql/backup/dhdv_$ts.bak F:\dhdv_stack_v_2.9\backup\
+Get-ChildItem F:\dhdv_stack_v_2.9\backup\dhdv_*.bak | ? LastWriteTime -lt (Get-Date).AddDays(-14) | Remove-Item -Force
